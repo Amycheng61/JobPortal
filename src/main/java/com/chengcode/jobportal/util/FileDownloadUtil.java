c381a8facdbc3269652628c23ea7594f1c722817
Amycheng61 <chengcaicai1234@gmail.com>
2026-02-08 20:35:28 +0100

    download candidate Resume

---
+++ src/main/java/com/chengcode/jobportal/util/FileDownloadUtil.java
@@ -0,0 +1,26 @@
+package com.chengcode.jobportal.util;
+
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDownloadUtil{
+    private Path foundPath;
+    public Resource getFileAsResourse(String downloadDir, String fileName)throws IOException {
+        Path path= Paths.get(downloadDir);
+        Files.list(path).forEach(file->{
+            if(file.getFileName().toString().startsWith(fileName)){
+                foundPath = file;
+            }
+        });
+        if(foundPath!=null){
+            return  new UrlResource(foundPath.toUri());
+        }
+        return null;
+    }
+}
